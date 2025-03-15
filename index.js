9adbffd1654d42e30ecb8da77e380d4c8aba9273
Joni Niemelä <jonimikael1234@gmail.com>
2025-03-15 22:50:57 +0200

    Exercise 3.1 completed

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: "2",
+        name: "Joni N.",
+        number: "085049830",
+    },
+    {
+        id: "3",
+        name: "John Smith",
+        number: "84324242",
+    },
+    {
+        id: "4",
+        name: "Erkki Esimerkki",
+        number: "04324242376",
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>API TOIMII :)</h1>')
+})
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        response.json(person);
+    }   else {
+        response.status(404).end();
+    }
+})
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
